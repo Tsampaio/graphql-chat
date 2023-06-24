973ed419365520c3a2459c0d67f4db9c9fcb11aa
Telmo Sampaio <telmoasampaio@gmail.com>
2023-06-24 21:53:53 +0200

    feat: setup apollo server

---
+++ index.js
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from "apollo-server";
+
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
