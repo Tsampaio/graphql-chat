72e695e898637c9172100316aa0d4006acd7df65
Telmo Sampaio <telmoasampaio@gmail.com>
2023-06-24 22:12:06 +0100

    query and variables

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const users = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Smith",
+    email: "john@email.com",
+    password: "passw45645ord",
+  },
+  {
+    id: 2,
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane@email.com",
+    password: "23423423",
+  },
+];
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    users: [User]
+    user(id:ID!): User
+  }
+
+  type User {
+    id: ID!
+    firstName: String
+    lastName: String
+    email: String
+    password: String
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    users: () => users,
+    user: (parent, { id }, context) => {
+      console.log(id);
+      return users.find((user) => user.id === parseInt(id));
+    }
   },
 };
 
